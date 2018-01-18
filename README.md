eb1b87a680522723e8eb7641d941369a4f1298b0
johnhw <johnhw@gmail.com>
2018-01-18 14:34:30 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,260 @@
-# blender_knots
-Blender plugin to create 3D meshes of knots from ASCII art descriptions
+# Quick knots for Blender 2.7+
+
+
+             +--+
+        >----|-----+                            
+             |  +--------<
+             |     |
+             |  +-------->
+        <----|-----+
+             +--+
+
+Blender plugin to create 3D meshes of knots from ASCII art descriptions. This is a simple plugin,
+which allows the creation of a knot mesh quickly. It generates Blender spline curves from the
+text, which can be extruded to build full 3D meshes. Generating a "good looking" knot with
+appropriate tension and curvature takes a bit of manual editing, but it is usually quick to do.
+
+## Example renders
+<img src="imgs/examples.png">
+
+
+
+# Installing
+* Open **Blender**. You'll need 2.70 or greater.
+* Go to `File/User Preferences` and choose the `Install from File` button at the bottom. Find `blender_knots.py` from
+the download and select it. Make sure you tick the `Development: Knot generator` setting, then save the user settings.
+
+The knot generator is now available in the `Create` tab in `Object` mode of the 3D view (below the buttons for the various
+standard meshes). You need to have a text fragment with a knot description in it, which you will be able to select.
+<img src="imgs/panel.png">
+
+## UI
+* `knot` select a text fragment (entered in the Blender text editor) to generate the knot from
+* `extrude` if enabled, this will turn on beveling for the knot. These options are provided to
+generate a good looking knot quickly -- you can get exactly the same effect by turning on bevel and
+adding the modifiers yourself.
+    * `width` width of the beveling for the knot path
+    * `smoothing steps` if non-zero will add a smoothing modifier
+    * `subdivision` if non-zero will add a subdivision surface modifier
+    
+* `Over/Under z-shift` These control how the geometry should look where knot elements cross.
+    * `depth` how much to move the rope going under over up/down where it crosses
+    * `bias` whether the undergoing rope moves down (bias = 1), the overgoing rope goes up (bias=-1),
+    or the undergoing rope goes down by 0.5 and the undergoing rope up by 0.5 (bias=0.5)
+    
+* `Make Knot` creates the geometry
+
+## What's included
+* `knot_plugin.py` The plugin itself
+* `knot_blank.blend` An empty template scene which you can generate a standard knot from a text file
+* `physics.blend` A simple example showing the use of physics to animate a knot tightening.
+
+### Editing
+The curve can most easily be edited with *proportional editing* turned on and set to *Connected* which
+will allow the vertices to be moved with some degree of continuity. The edit mode tool `Smooth` (in the 
+Tools panel) can also be used to tweak the generated vertices to make them lie a bit more smoothly.
+
+Note: if you want to separate ropes into different objects (e.g. to render ropes in different colours),
+then select a rope in edit mode using `Select/Linked` and then use `Curve/Separate` to separate that rope
+into a new object. This is best done after the knot is fully posed.
+
+### Animating
+Using curves has the nice property that the curves can be animated using the soft body physics to
+simulate the knot movement. It's tricky to get realistic motion where the rope doesn't self intersect, but
+it can be used in simple cases, and can be used to drape the rope over objects.
+
+To control the movement of the rope, use Hooks to pull on the curve. The easiest way to set this is up
+is to go to `edit mode` (TAB), select the vertex you want to drag, then `Curve/Control Points/Hooks/Hook to New Object` to 
+create a new hook that can pull the rope. This will generate an Empty that
+can be keyframed in the usual way to animate motion of the rope with physics turned on.
+
+
+
+## Notation:
+
+The notation is a simple ASCII art representation of the path of the rope. The rope
+can go in each of the four cardinal directions, and can go beneath existing rope.
+
+Every knot is represented as a collection of **leads**, where one a **lead** is a separate
+piece of rope. A simple overhand knot only has one lead; a bend will have at least two lead, and so on.
+
+
+* **lead** a connected sequence representing one physical strand of rope 
+* **space** any cell without any character
+
+* Every line must have a **head**; either directed, undirected or numbered
+* A line may have an explicit **tail** (ending marker), either directed or undirected but this is optional
+
+## Examples
+
+    
+ 
+### Reef knot
+
+                 +--+
+        >--------|-----+                            
+                 |  +----------<
+                 |     |
+                 |  +---------->
+        <--------|-----+
+                 +--+
+                 
+                 
+             
+### Figure of 8
+        
+        
+               +--+
+              |  | 
+        >-----|-----+
+              |  |  |
+              +--|----->
+                 |  |
+                 +--+
+                 
+                 
+### Bowline
+        
+        
+              V
+              |
+              |
+             /|-\
+             \-\|
+            /--||-\
+            | \---/
+            |  ||
+            |  V|
+            \---/
+                        
+        
+
+
+## Characters
+
+### Leads
+* `- |` connected elements of a line
+
+
+        ---------
+
+
+        |
+        |
+        |
+        
+        
+#### Under/over
+A line is assumed to continue underneath if it disappears without a corner symbol:
+    
+    
+        V                   V
+        |                   |
+    >---|---             >------
+        |                   |
+    
+    
+This may continue for any number of steps:
+    
+         VVV
+         |||
+    >----|||----
+         |||
+         |||
+
+
+                          
+
+### Corners         
+#### Undirected
+* `+` undirected corner
+
+  Indicates a change of direction. Must be unambiguous when following the path of the lines
+  
+  
+      >----+
+           |
+           +----
+
+                 |
+       >--------++-<
+                |
+
+        NOT OK:
+
+                  |
+            >-----+
+                  |
+
+                            
+#### Directed
+* `/ \` directed corner; Can be used to explicitly annotate direction of corner
+
+                 |
+                 |
+        >--------/       >----\
+                              |
+                 
+        
+### Heads/tails
+#### Directed
+* `^ v > <` Directed head
+
+Indicates head of a lead, if facing a lead, or the tail if facing whitespace
+
+        >---
+
+        --->
+
+        V
+        |
+        |
+        V
+    
+#### Undirected 
+* `[0-9]` undirected head
+
+Indicates start of a lead. Labels a lead with the given number. Only one digit permitted. Must neighbour exactly one line cell so that direction is indicated.
+
+        0---
+
+        0
+        |
+        |
+        
+
+
+* `.` Undirected tail (optional)
+
+        ---.
+
+        |
+        |
+        .
+        
+## Comments/names
+* `[name]` names a line
+
+May appear anywhere on the line excluding the end, including before the leader or in place of the leader. Always read left to right, regardless of line orientation. The lead must pass through the name to take effect.  Can be used as a comment if not adjacent to a lead.
+
+
+
+    [a comment; this --- is ignored]
+    
+               V
+               |
+    [this has a lead through it and will name the lead]
+               |
+               .
+                           
+    
+            >[left]---> <---[right]<
+
+            0
+            |
+            |
+         [left]
+            |
+            |
+            V
+
---
+++ knot_plugin.py
@@ -0,0 +1,520 @@
+bl_info = {
+    "name": "Knot generator",
+    "description": "",
+    "author": "John H. Williamson",
+    "version": (0, 0, 1),
+    "blender": (2, 7, 0),
+    "location": "3D View > Tools",
+    "warning": "", # used for warning icon and text in addons panel
+    "wiki_url": "",
+    "tracker_url": "",
+    "category": "Development"
+}
+
+### Knot parsing
+
+from collections import defaultdict
+
+char_dirs = {"^":(0,-1), "V":(0,1), ">":(1,0), "<":(-1,0), "O":(0,0)}
+inv_dirs = {v:k for k,v in char_dirs.items()}
+
+def nonempty(kmap, x, y):
+        return [(x_off, y_off) 
+                for x_off,y_off in char_dirs.values()
+                if (x+x_off, y+y_off) in kmap]
+    
+# ^V>< move in this direction
+# # invalid direction; this is an error
+# U continue in current direction, but go underneath
+# O stop; end of lead
+# C check neighbours; must only be one neighbour not equal to current direction; take that
+table = """
+.   O ^ V > <
+O   C # # # #
+.   # . . . .
+^   ^ ^ # U U
+V   V # V U U
+>   > U U > #
+<   < U U # <
+-   # U U > <
+|   # ^ V U U
+/   # > < ^ V
+\   # < > V ^
++   # C C C C        
+L   # L L L L
+"""
+
+# compute the table of possibilities
+follow_map = {}
+for line in table.splitlines():
+    base_dirs = "O^V><"
+    if len(line)>1:        
+        chars = line.split()
+        in_char,out_chars = chars[0], chars[1:]
+        for i, c in enumerate(base_dirs):
+            follow_map[(in_char, base_dirs[i])] = out_chars[i]
+            
+class KnotException(Exception):
+    pass
+
+class Knot:
+    def parse_map(self, s):
+        # convert from the ascii string to a dictionary of coordinates
+        self.map = {}
+        self.inv_map = defaultdict(list)
+        self.labels = {}       
+        
+        # represents a label object, that many
+        # cells might refer to
+        class Label:
+            def __init__(self):
+                self.label = ""
+            def append(self, c):
+                self.label += c            
+                
+        for y, line in enumerate(s.splitlines()):
+            mark_invalid = False # clear invalid area flag
+            for x, char in enumerate(line):
+                
+                if not mark_invalid:
+                    # label 
+                    if char=='[':
+                        mark_invalid = True
+                        label = Label()   # new label object                     
+                    if not char.isspace():
+                        self.map[(x,y)] = char
+                        self.inv_map[char].append((x,y))
+                else:                    
+                    if char==']':
+                        # label finished; record it
+                        mark_invalid = False
+                        #self.labels[label_start] = line[label_start[0]+1:x]                    
+                    else:
+                        # mark these parts of the map as invalid
+                        self.map[(x,y)] = 'L'
+                        self.inv_map['L'].append((x,y))
+                        label.append(char)
+                        self.labels[(x,y)] = label
+                    
+                        
+    def choose(self, x, y, dx, dy):
+        """Return the neighbouring point that is going in a different direction,
+        or throw an error if there is none or more than one."""
+        neighbours = nonempty(self.map, x, y)        
+        valid = [(vx, vy) for vx,vy in neighbours if not(vx==-dx and vy==-dy) and not(vx==0 and vy==0)]  
+        
+        if len(valid)<1:
+            self.print_error(x,y, "No neighbour to turn to")              
+        if len(valid)>1:
+            self.print_error(x,y,"Ambiguous neighbour")            
+        return valid[0]
+    
+    def find_heads(self):
+        """Find each starting head"""                    
+        heads = []
+        
+        # find all potential heads
+        for char, (x_off,y_off) in char_dirs.items():
+            locations = self.inv_map[char]
+            for x,y in locations:                                
+                if x_off==0 and y_off==0:
+                    # undirected head
+                    x_off, y_off = self.choose(x,y,0,0)
+                    heads.append((x,y,x_off,y_off))
+                else:
+                    # check if this is a head (no space beforehand)
+                    prev_lead = self.map.get((x-x_off, y-y_off), None)                        
+                    if prev_lead is None:                        
+                        heads.append((x,y,x_off,y_off))
+                        
+        # sort by y then x
+        return sorted(heads, key=lambda x:(x[1], x[0]))
+        
+    def print_error(self, x, y, msg=""):
+        """Print out a message with an error about the knot being parsed"""
+        k = 3
+        n = 6
+        print(msg.center(n*2))
+        for i in range(-n,n+1):
+            for j in range(-n, n+1):
+                if (abs(j)==k and abs(i)<k) or (abs(j)<k and abs(i)==k):
+                    print("@", end="")
+                else:
+                    char = self.map.get((x+j,y+i))
+                    char = char or " "
+                    print(char, end="")
+            print()
+        raise KnotException(msg)
+    
+    def print_map(self, kmap):      
+        """Print out the entire map, in canonical form."""
+        def vrange(ix):
+            vs = [pt[ix] for pt in kmap.keys()]
+            return min(vs), max(vs)
+        min_x, max_x = vrange(0)
+        min_y, max_y = vrange(1)
+        for i in range(min_y, max_y+1):
+            for j in range(min_x, max_x+1):
+                char = kmap.get((j,i))
+                char = char or " "
+                print(char, end="")
+            print()                
+                                
+    
+    def trace_leads(self):
+        heads = self.find_heads()      
+        self.lead_map = defaultdict(list)
+        self.leads = []
+        for head in heads:
+            lead = []
+            x,y,dx,dy = head
+            z = 0
+            name = ""
+            lead.append((x,y,dx,dy,z,name))
+            x,y = x+dx, y+dy
+            while (x,y) in self.map:                
+                char = self.map.get((x,y))                
+                # where we are going now (as a character)
+                dir_char = inv_dirs[(dx,dy)]
+                
+                action = follow_map.get((char, dir_char))
+                # simple change of direction
+                if action in char_dirs:
+                    dx, dy = char_dirs[action]
+                    z = 0
+                elif action=='L':
+                    # rename lead
+                    name = self.labels[(x,y)].label
+                elif action=='U':
+                    # dx,dy don't change
+                    z = -1
+                elif action=='C':
+                    dx, dy = self.choose(x,y,dx,dy)
+                    z = 0
+                elif action=='.':
+                    break
+                elif action=='#':
+                    self.print_error(x,y, "Invalid direction") 
+                elif action is None:
+                    self.print_error(x,y,"Character %s unexpected"%char)
+                lead.append((x,y,dx,dy,z,name))
+                self.lead_map[(x,y)].append((dx,dy,z,name))
+                
+                x, y = x+dx, y+dy
+            self.leads.append(lead)
+        print(self.lead_map)
+               
+    def is_crossing(self, x, y):
+        print((x,y))
+        cross = self.lead_map.get((x,y))
+        return cross is not None and len(cross)>1
+    
+                 
+    def show_lead(self, lead):
+        kmap = {}
+        for x,y,dx,dy,z,name in lead:
+            if dx==0 and (x,y) not in kmap:
+                kmap[(x,y)] = '|'
+            elif  (x,y) not in kmap:
+                kmap[(x,y)] = '-'
+            elif (x,y) in kmap:
+                kmap[(x,y)] = '+'            
+                        
+        self.print_map(kmap)
+        
+    def __init__(self, s):
+        """Take a string representation of a knot, and fill 
+        in the knot data from the string."""
+        # generate map of characters
+        self.parse_map(s)
+        self.trace_leads()
+       
+        
+        
+### END knot parsing
+
+### Blender Python interface
+import bpy
+
+from bpy.props import (StringProperty,
+                       BoolProperty,
+                       IntProperty,
+                       FloatProperty,
+                       EnumProperty,
+                       PointerProperty,
+                       )
+from bpy.types import (Panel,
+                       Operator,
+                       PropertyGroup,
+                       )
+
+
+
+
+# ------------------------------------------------------------------------
+#    store properties in the active scene
+# ------------------------------------------------------------------------
+
+class KnotSettings(PropertyGroup):
+    
+    knot_text = StringProperty(
+        name="Knot",
+        description="Select knot to choose",
+        default = ""
+        )
+    
+
+    extrude = BoolProperty(
+        name="Extrude",
+        description="Extrude knot; otherwise no bevel is applied to the curve",
+        default = True
+        )
+        
+    extrude_width = FloatProperty(
+        name="Width",
+        description="Extrusion (bevel) width",
+        default = 0.3,
+        min=0.0,
+        max=10.0
+        )
+
+    curve = BoolProperty(
+        name="Create Curve",
+        description="Create curve (otherwise, create a plain mesh)",
+        default=True)
+        
+    smoothing = IntProperty(
+        name = "Smoothing steps",
+        description="Number of smoothing steps to apply after knot generated",
+        default = 5,
+        min = 0,
+        max = 30
+        )
+        
+    subdiv = IntProperty(
+        name = "Subdivision",
+        description="Number of subdivisions to apply",
+        default = 5,
+        min = 0,
+        max = 30
+        )
+
+    z_depth = FloatProperty(
+        name = "Depth",
+        description="Z shift at crossovers",
+        default = 1,
+        min = 0,
+        max = 10
+        )
+        
+    z_bias = FloatProperty(
+        name = "Bias",
+        description="Z bias at crossovers (-1=over rises, 0=split, 1=under lowers)",
+        default = 0,
+        min = -1,
+        max = 1
+        )
+        
+    knot_text = StringProperty()
+
+    my_float = FloatProperty(
+        name = "Float Value",
+        description = "A float property",
+        default = 23.7,
+        min = 0.01,
+        max = 30.0
+        )
+
+    my_string = StringProperty(
+        name="User Input",
+        description=":",
+        default="",
+        maxlen=1024,
+        )
+
+    my_enum = EnumProperty(
+        name="Dropdown:",
+        description="Apply Data to attribute.",
+        items=[ ('OP1', "Option 1", ""),
+                ('OP2', "Option 2", ""),
+                ('OP3', "Option 3", ""),
+               ]
+        )
+
+# ------------------------------------------------------------------------
+#    operators
+# ------------------------------------------------------------------------
+
+
+
+import bpy
+from bpy.types import Operator
+from bpy.props import FloatVectorProperty
+from bpy_extras.object_utils import AddObjectHelper, object_data_add
+from mathutils import Vector
+    
+def add_knot(self, context, knot_string, z_scale, bias, name="Knot"):
+    
+    ix = 0
+    verts = []
+    edges = []
+    
+    knot_obj = Knot(knot_string)
+    
+    for lead in knot_obj.leads:
+        prev = None
+        for x,y,dx,dy,z,name in lead:
+            if knot_obj.is_crossing(x,y):
+                if z==-1:
+                    verts.append(Vector((x,y,-z_scale * (bias+1)/2)))
+                else:
+                    verts.append(Vector((x,y,z_scale*  (bias+1)/2)))                    
+            else:
+                verts.append(Vector((x,y,0)))
+                
+            if prev is not None:
+                edges.append((prev, ix))
+            prev = ix
+            ix += 1
+        #if prev is not None:
+        #    edges.append((prev, ix))
+        
+            
+    mesh = bpy.data.meshes.new(name=name)
+    mesh.from_pydata(verts, edges, [])
+    # useful for development when the mesh may be invalid.
+    mesh.validate(verbose=True)
+    object_data_add(context, mesh, operator=self)
+    
+    
+class KnotOperator(bpy.types.Operator,AddObjectHelper):
+    bl_idname = "wm.make_knot"
+    bl_label = "Make Knot"
+
+    def execute(self, context):
+        scene = context.scene
+        knottool = scene.knot_tool
+
+        # print the values to the console
+        print("Make Knot")
+        print("extrude state:", knottool.extrude)
+        print("knot:", knottool.knot_text)
+        knot = bpy.data.texts[knottool.knot_text].as_string()
+        print(knot)
+        
+        ######
+
+        
+        add_knot(self, context, knot, knottool.z_depth, knottool.z_bias, knottool.knot_text)
+        if knottool.curve:
+            bpy.ops.object.convert(target="CURVE")
+
+            active = bpy.context.object
+            curves = active.data
+            
+            curves.fill_mode = 'FULL'
+            curves.bevel_depth = knottool.extrude_width       
+            curves.bevel_resolution = 6
+            curves.use_uv_as_generated = True
+        
+        if knottool.smoothing>0:
+            bpy.ops.object.modifier_add(type="SMOOTH")
+            active.modifiers['Smooth'].iterations = knottool.smoothing        
+            
+        if knottool.subdiv>0:
+            bpy.ops.object.modifier_add(type="SUBSURF")
+            active.modifiers['Subsurf'].levels = knottool.subdiv
+            active.modifiers['Subsurf'].render_levels = knottool.subdiv
+
+        
+        saved_location = bpy.context.scene.cursor_location.copy()  # returns a copy of the vector    
+        bpy.ops.object.mode_set(mode = 'EDIT')    
+        if knottool.curve:
+            bpy.ops.curve.select_all(action='SELECT')        
+        else:
+            bpy.ops.mesh.select_all(action='SELECT')        
+        bpy.ops.view3d.snap_cursor_to_selected()
+        bpy.ops.object.mode_set(mode = 'OBJECT')
+        # set the origin on the current object to the 3dcursor location
+        bpy.ops.object.origin_set(type='ORIGIN_CURSOR')
+        # set 3dcursor location back to the stored location
+        bpy.context.scene.cursor_location = saved_location
+
+        return {'FINISHED'}
+
+
+
+# The menu can also be called from scripts
+#bpy.ops.wm.call_menu(name=KnotTextItemMenu.bl_idname)
+
+# ------------------------------------------------------------------------
+#    my tool in objectmode
+# ------------------------------------------------------------------------
+
+class OBJECT_PT_my_panel(Panel):
+    bl_idname = "OBJECT_PT_knot"
+    bl_label = "Knot"
+    bl_space_type = "VIEW_3D"   
+    bl_region_type = "TOOLS"    
+    bl_category = "Create"
+    bl_context = "objectmode"   
+
+    @classmethod
+    def poll(self,context):
+        return True
+
+    def draw(self, context):
+        layout = self.layout
+        scene = context.scene
+        myknot = scene.knot_tool
+
+        layout.prop_search(context.scene.knot_tool, 'knot_text', bpy.data, 'texts', text="Knot")
+        
+        layout.prop(myknot, "curve")
+        
+        #####
+
+        box = layout.box()
+        box.label("Extrude")
+
+        
+        box.prop(myknot, "extrude")
+        box.prop(myknot, "extrude_width")
+        box.prop(myknot, "smoothing")
+        box.prop(myknot, "subdiv")
+        
+        ####        
+        box = layout.box()
+        box.label("Over/Under Z shift")
+        box.prop(myknot, "z_depth")
+        box.prop(myknot, "z_bias")
+
+        ####        
+        
+        
+      
+#        layout.prop(mytool, "my_enum", text="") 
+#        layout.prop(mytool, "my_int")
+#        layout.prop(mytool, "my_float")
+#        layout.prop(mytool, "my_string")
+
+        ####
+        layout.operator("wm.make_knot")
+
+
+# ------------------------------------------------------------------------
+# register and unregister
+# ------------------------------------------------------------------------
+
+def register():
+    bpy.utils.register_module(__name__)
+    bpy.types.Scene.knot_tool = PointerProperty(type=KnotSettings)
+
+
+
+def unregister():
+    bpy.utils.unregister_module(__name__)
+    del bpy.types.Scene.knot_tool
+
+if __name__ == "__main__":
+    register()
